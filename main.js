7b59603b5072161ce1a288292995283069e81784
Nick Eddy <nick.a.eddy@gmail.com>
2017-04-18 21:26:40 -0700

    added function to check which arrow was pressed.

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-console.log("test");
+var gameBoard = {
+    a:[0,0,0,0],
+    b:[0,0,0,0],
+    c:[0,0,0,0],
+    d:[0,0,0,0]
+}
+
+
+var view = {
+    setUpEventListeners: function() {
+        document.addEventListener("keydown", function(event) {
+           view.checkKey(event.which); 
+        });
+    },
+    checkKey: function(keyvalue) {
+        switch(keyvalue) {
+            case 37:
+                console.log("left");
+                break;
+            case 38:
+                console.log("up");
+                break;
+            case 39:
+                console.log("right");
+                break;
+            case 40:
+                console.log("down");
+                break;
+            default:
+                console.log("other key with value " + keyvalue);
+                break;
+        }
+    }
+}
+view.setUpEventListeners();
